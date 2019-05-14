832f51207a10705fa2904ed8dccb5e37fb8f2e9b
Supreeth <Supreeth@DESKTOP-78CHICM>
2019-05-14 08:00:15 +0530

    first message

---
+++ src/Git/demo1.java
@@ -0,0 +1,13 @@
+package Git;
+
+public class demo1 {
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("first time");
+	
+	}
+
+}
